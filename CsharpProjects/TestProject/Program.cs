7b2d7d1a95084d21569126829183bfcc7017a3e6
Michael Amaya <michael.amaya720@gmail.com>
2024-06-07 01:37:48 -0700

    changing source project for editing

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,31 +1,5 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿string source = "scuffedev";
 
-if (daysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subsciption expires within a day!");
-    discountPercentage += 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subsciption expire in {daysUntilExpiration} days.");
-    discountPercentage += 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-else
-{
-    Console.WriteLine();
-}
+string outputSource = $@"C:home\{source}\GitHub";
+Console.WriteLine($"I need you to input the source on:\n\t{outputSource}");
 
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%!");
-}
